70486c26473e483da1c8294c76c06ca14a0fb02a
Dan Salo <dancsalo@gmail.com>
2019-02-18 11:46:17 -0500

    Add new models.

--- models.py
+++ models.py
@@ -93,4 +93,101 @@ class TwoLayerMLP(ClassyClassifier):
         label_scores = self.decoder(text_embedding_tensors).squeeze()
         return label_scores
 
+
+class TwoLayerMLPdp(ClassyClassifier):
+
+    def __init__(self, *args, **kwargs):
+        super(TwoLayerMLPdp, self).__init__(*args, **kwargs)
+
+    def init_embedder(self) -> Embeddings:
+        """Initializes the sentence embedding object"""
+        return StackedEmbeddings(self.embeddings)
+
+    def init_layers(self):
+        """Initializes the layers to be used in forward()"""
+        hidden = 128
+        self.decoder = torch.nn.Sequential(
+                torch.nn.Linear(self.embedding_length, hidden),
+                torch.nn.ReLU(),
+                torch.nn.Dropout(0.5),
+                torch.nn.Linear(hidden, len(self.label_dictionary))
+            )
+
+    def forward(self, sentences: Union[List[Sentence], Sentence]) -> torch.Tensor:
+        """Defines one forward pass of the neural net"""
+        self.embedder.embed(sentences)
+
+        # Average
+        text_embedding_tensors = torch.stack(
+            [torch.stack([word.embedding for word in sentence]).mean(dim=0)
+             for sentence in sentences])
+
+        label_scores = self.decoder(text_embedding_tensors).squeeze()
+        return label_scores
+
+
+class TwoLayerMLPbn(ClassyClassifier):
+
+    def __init__(self, *args, **kwargs):
+        super(TwoLayerMLPbn, self).__init__(*args, **kwargs)
+
+    def init_embedder(self) -> Embeddings:
+        """Initializes the sentence embedding object"""
+        return StackedEmbeddings(self.embeddings)
+
+    def init_layers(self):
+        """Initializes the layers to be used in forward()"""
+        hidden = 128
+        self.decoder = torch.nn.Sequential(
+                torch.nn.Linear(self.embedding_length, hidden),
+                torch.nn.BatchNorm1d(hidden, affine=False),
+                torch.nn.ReLU(),
+                torch.nn.Linear(hidden, len(self.label_dictionary)),
+            )
+
+    def forward(self, sentences: Union[List[Sentence], Sentence]) -> torch.Tensor:
+        """Defines one forward pass of the neural net"""
+        self.embedder.embed(sentences)
+
+        # Average
+        text_embedding_tensors = torch.stack(
+            [torch.stack([word.embedding for word in sentence]).mean(dim=0)
+             for sentence in sentences])
+
+        label_scores = self.decoder(text_embedding_tensors).squeeze()
+        return label_scores
+
+
+class TwoLayerMLPbndp(ClassyClassifier):
+
+    def __init__(self, *args, **kwargs):
+        super(TwoLayerMLPbndp, self).__init__(*args, **kwargs)
+
+    def init_embedder(self) -> Embeddings:
+        """Initializes the sentence embedding object"""
+        return StackedEmbeddings(self.embeddings)
+
+    def init_layers(self):
+        """Initializes the layers to be used in forward()"""
+        hidden = 128
+        self.decoder = torch.nn.Sequential(
+                torch.nn.Linear(self.embedding_length, hidden),
+                torch.nn.ReLU(),
+                torch.nn.BatchNorm1d(hidden),
+                torch.nn.Dropout(0.5),
+                torch.nn.Linear(hidden, len(self.label_dictionary)),
+            )
+
+    def forward(self, sentences: Union[List[Sentence], Sentence]) -> torch.Tensor:
+        """Defines one forward pass of the neural net"""
+        self.embedder.embed(sentences)
+
+        # Average
+        text_embedding_tensors = torch.stack(
+            [torch.stack([word.embedding for word in sentence]).mean(dim=0)
+             for sentence in sentences])
+
+        label_scores = self.decoder(text_embedding_tensors).squeeze()
+        return label_scores
+
 # Add additional models below #
